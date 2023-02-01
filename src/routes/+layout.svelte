1a7cbd6b5b7c866bceb094d642a52211f1684dec
kiryanchi <kiryanchi@naver.com>
2023-02-01 22:59:41 +0900

    fix(0.2): 페이지 이동해도 날짜가 유지되도록 수정

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,23 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import { getToday } from '$lib/util/date';
 	import '../app.css';
 
 	const today = getToday();
+	const date = $page.url.toString().split('/').reverse()[0];
 </script>
 
 <header class="bg-white mb-[8vh]">
 	<nav class="flex flex-col justify-between items-center w-[92%] my-auto">
 		<div class="mt-4">
 			<h1 class="text-6xl w-auto">
-				<a class="hover:text-black" href="/{today}">일백일준</a>
+				<a class="hover:text-black" href="/{date}">일백일준</a>
 			</h1>
 		</div>
 		<div class="flex flex-row mt-4">
 			<ul class="flex flex-row items-center gap-[4vw]">
 				<li class="">
-					<a class="hover:text-gray-500" href="/{today}">문제 풀이 현황</a>
+					<a class="hover:text-gray-500" href="/{date}">문제 풀이 현황</a>
 				</li>
 				<li class="">
 					<a class="hover:text-gray-500" href="/help">도움말</a>
