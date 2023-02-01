43e907d3c7ea9cffc07ae62857e84b63312d4da8
kiryanchi <kiryanchi@naver.com>
2023-02-01 23:23:55 +0900

    feat(0.2): 문제 풀이 현황 CSS 적용

--- src/routes/[date=date]/+page.svelte
+++ src/routes/[date=date]/+page.svelte
@@ -5,67 +5,73 @@
 	export let data: PageData;
 </script>
 
-<h1>{data.date} 문제 풀이!!</h1>
+<div class="w-[50%] ml-[21%]">
+	<div class="flex justify-between w-[50%] text-4xl ml-[25%] mb-4">
+		<p>◁</p>
+		<p class="font-bold underline">{data.date}</p>
+		<p>▷</p>
+	</div>
 
-<Card body>
-	<Container>
-		<Row>
-			<Col>
-				<h4>이름</h4>
-			</Col>
-			<Col>
-				<h4>실천 여부</h4>
-			</Col>
-		</Row>
-	</Container>
-	<Container>
-		{#each [...data.solvingByUser] as [name, solving]}
+	<Card body class="font-bold text-center mb-4">
+		<Container>
 			<Row>
 				<Col>
-					{name}
+					<h4>이름</h4>
 				</Col>
 				<Col>
-					{#if solving.length != 0}
-						<p>✅</p>
-					{:else}
-						<p>❌</p>
-					{/if}
+					<h4>실천 여부</h4>
 				</Col>
 			</Row>
-		{/each}
-	</Container>
-</Card>
+		</Container>
+		<Container>
+			{#each [...data.solvingByUser] as [name, solving]}
+				<Row>
+					<Col>
+						{name}
+					</Col>
+					<Col>
+						{#if solving.length != 0}
+							<p>✅</p>
+						{:else}
+							<p>❌</p>
+						{/if}
+					</Col>
+				</Row>
+			{/each}
+		</Container>
+	</Card>
 
-<Card body>
-	<Container>
-		<Row>
-			<Col>
-				<h4>이름</h4>
-			</Col>
-			<Col>
-				<h4>풀었던 문제</h4>
-			</Col>
-		</Row>
-	</Container>
-	<Container>
-		{#each [...data.solvingByUser] as [name, solvings]}
+	<Card body class="font-bold text-center">
+		<Container>
 			<Row>
 				<Col>
-					{name}
+					<h4>이름</h4>
 				</Col>
 				<Col>
-					{#if solvings.length != 0}
-						{#each solvings as { date, difficulty, title, url }}
-							<a href={url}><p>{`${difficulty} ${title}`}</p></a>
-						{/each}
-					{:else}
-						<p>❌</p>
-					{/if}
+					<h4>풀었던 문제</h4>
 				</Col>
 			</Row>
-		{/each}
-	</Container>
-</Card>
+		</Container>
+		<Container>
+			{#each [...data.solvingByUser] as [name, solvings]}
+				<Row>
+					<Col>
+						{name}
+					</Col>
+					<Col>
+						{#if solvings.length != 0}
+							{#each solvings as { date, difficulty, title, url }}
+								<a href={url}><p>{`${difficulty} ${title}`}</p></a>
+							{/each}
+						{:else}
+							<p>❌</p>
+						{/if}
+					</Col>
+				</Row>
+			{/each}
+		</Container>
+	</Card>
+</div>
 
 <style>
 </style>
