0aecd4d6e926cc317579c523de17ece79d98f875
kiryanchi <kiryanchi@naver.com>
2023-02-01 07:26:48 +0000

    feat(0.1): 배포

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,9 @@
 	import { getToday } from '$lib/util/date';
 	import { getContext, hasContext, setContext } from 'svelte';
 	import type { PageData } from './$types';
-	import Calendar from './components/Calendar.svelte';
 
 	const dateQuery = $page.url.searchParams.get('date');
 
 	const date: String = dateQuery ? (dateQuery as String) : getToday();
 </script>
 
-<Calendar {date} />
